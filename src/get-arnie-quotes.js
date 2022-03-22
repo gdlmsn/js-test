67765064ecc377f115e950a5cea7c5ce643c7e22
gdlmsn <gdalmasson@gmail.com>
2022-03-22 19:37:29 +1100

    get arnie quotes function implementation

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,9 +1,18 @@
 const { httpGet } = require('./mock-http-interface');
 
+async function fetch(url){
+  try {
+    const response = await httpGet(url);
+    return {[response.status === 200 ? 'Arnie Quote' : 'FAILURE'] : JSON.parse(response.body).message}
+  } catch (error) {
+    return console.log('err', error.message)
+  }
+}
+
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
-};
+    const results = await Promise.all(urls.map((url)=>fetch(url)));
+    return results;
+  };
 
 module.exports = {
   getArnieQuotes,
